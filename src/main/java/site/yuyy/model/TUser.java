7e2cb332b65abf71c82de825845433505325be97
yuyiyong <1546621894@qq.com>
2018-01-02 18:16:26 +0800

    aaa

---
+++ src/main/java/site/yuyy/model/TUser.java
@@ -0,0 +1,35 @@
+package site.yuyy.model;
+
+import java.util.Date;
+
+public class TUser {
+    private Integer id;
+
+    private String userName;
+
+    private Date createTime;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName == null ? null : userName.trim();
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+}
